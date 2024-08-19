a154f0db2c1ab17eaaa79044535aefb91b85e2af
josevilson <45457805+josevilson@users.noreply.github.com>
2024-08-19 19:58:01 -0300

    Delete todo.py
    Delete todo

--- todo.py
+++
@@ -1,5 +0,0 @@
-# separa data (diam, mes, ano) -> ok
-# funcao de agrupamento (falta adicionar tipo) e ajustar o bug por data.
-# adicionar typehint e docstring
-# atualizar registro -> falta ajustar o indice que não existe.
-# valicoes de input no geral
